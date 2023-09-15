c05a569ba10e5ef06ccc808c6d17565789b21a84
4eh711 <Cheh711@gmail.com>
2023-09-16 02:32:45 +0300

    Nejro_Network 228

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ cat_face = cat_face_cascade.detectMultiScale(image)
 cat = Image.open(image_path)
 glasses = Image.open('glasses.png')
 smile = Image.open('smile.png')
-cap = Image.open('cap_1.png')
+#cap = Image.open('cap_1.png')
 cat = cat.convert("RGBA")
 glasses = glasses.convert("RGBA")
 smile = smile.convert("RGBA")
-cap = cap.convert("RGBA")
+#cap = cap.convert("RGBA")
 
 print(cat_face)
 #for (x,y,w,h) in cat_face:
@@ -30,8 +30,8 @@ for (x,y,w,h) in cat_face:
     cat.paste(glasses, (x, int(y+h/4)), glasses)
     smile = smile.resize((int(w / 2), int(h / 4)))
     cat.paste(smile, (x+20, int(y/3 + h)), smile)
-    cap = cap.resize((w+50, h+20))
-    cat.paste(cap, (x-30, y-55), cap)
+    #cap = cap.resize((w+50, h+20))
+    #cat.paste(cap, (x-30, y-55), cap)
 
 
 cat.save("cat_with_glasses.png")
--- pattern1.py
+++ pattern1.py
@@ -1,2 +1,31 @@
 import cv2
+from PIL import Image
+
+image_cat = 'cat1.jpeg'
+
+cat_face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+image = cv2.imread(image_cat)
+
+cat_face = cat_face_cascade.detectMultiScale(image)
+print(cat_face)
+
+cat = Image.open(image_cat)
+glasses = Image.open('glasses.png')
+cat = cat.convert("RGBA")
+glasses = glasses.convert("RGBA")
+
+for (x,y,w,h) in cat_face:
+    glasses = glasses.resize((w, int(h / 3)))
+    cat.paste(glasses, (x, int(y + h / 4)), glasses)
+
+    cat.save("kit911.png")
+    kit911 = cv2.imread("kit911.png")
+    cv2.imshow("Кіт в окулярах", kit911)
+    cv2.waitKey()
+
+
+
+
+#cv2.imshow("Cat", image)
+#cv2.waitKey()
 
